ec0730a60a5a169d7a365b1ceb45bb88adb65d3c
Kal Sandbrook <kal.sandbrook1@gmail.com>
2024-04-09 12:45:54 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="GamePile API CLI Tool",
+    version="1.0",
+    description="The API Tool for GamePile, a University Major Project.",
+    author="Kal Sandbrook (kas143)",
+    author_email="kas143@aber.ac.uk",
+    packages="[GamePile-API]",
+    install_requires=['requests','thefuzz']
+)
